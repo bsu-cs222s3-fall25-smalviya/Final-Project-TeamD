dda8c4401b1c07c47606911aca16842cda142d12
Masonwp123 <MasonParker868@gmail.com>
2025-10-14 14:01:25 -0400

    Updated Data grabber to use financialmodelingprep

--- src/main/java/PaperTraderDataGrabber.java
+++ src/main/java/PaperTraderDataGrabber.java
@@ -1,8 +1,4 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.annotations.SerializedName;
+import com.google.gson.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,9 +12,9 @@ import java.util.*;
 
 public class PaperTraderDataGrabber {
 
-    private static final String APIKEY = "20CQ8O1XLIZ620N6";
+    private static final String APIKEY = "PQVbisd28MxeTJJSWyEVHCerZnNkbkvI";
 
-    private static final int startingPlace = 0; //0 to 20
+    private static final int startingPlace = 0; //0 to 42
 
     private static final Map<Integer, Integer> MONTHS_TO_DAYS = Map.ofEntries(
             Map.entry(1, 31),
@@ -39,6 +35,19 @@ public class PaperTraderDataGrabber {
             "NVDA","MSFT","AAPL","GOOGL","AMZN","META","AVGO","TSLA","TSM","BRK.B","ORCL","JPM","WMT","LLY","V","NFLX","MA","XOM","JNJ","PLTR","COST","ABBV","BABA","ASML","HD","AMD","BAC","PG","UNH","SAP","GE","CVX","KO","CSCO","IBM","AZN","NVO","NVS","TMUS","WFC","TM","MS","GS","PM","CAT","CRM","ABT","HSBC","AXP","RTX","MRK","LIN","SHEL","MU","SHOP","MCD","RY","APP","TMO","UBER","DIS","ANET","BX","PEP","NOW","T","PDD","SONY","INTU","BLK","ARM","LRCX","INTC","C","QCOM","MUFG","AMAT","NEE","VZ","GEV","SCHW","HDB","BKNG","BA","TXN","ISRG","AMGN","ACN","TJX","APH","SPGI","SAN","DHR","ETN","UL","PANW","GILD","BSX","ADBE","PFE","BHP","PGR","SYK","SPOT","TD","UNP","KLAC","COF","HOOD","LOW","HON","UBS","TTE","CRWD","MDT","DE","SNY","LMT","DASH","CEG","ADP","ADI","RIO","BUD","COP","CB","MELI","WELL","BTI","CMCSA","IBN","MO","SE","KKR","BBVA","PLD","ENB","VRTX","SO","SCCO","DELL","BN","SMFG","MMC","NKE","COIN","HCA","NTES","CVS","DUK","CME","BAM","TT","MCK","CDNS","PH","NEM","GD","MSTR","NOC","BMO","ICE","BMY","SBUX","SNPS","BP","MCO","WM","GSK","RBLX","AMT","ORLY","SNOW","RCL","SHW","RELX","CI","ELV","AEM","MMM","MFG","MDLZ","MRVL","CRH","EQIX","BNS","AON","ECL","CVNA","AJG","CTAS","WMB","NET","PBR","PBR.A","HWM","MSI","PNC","BK","NU","CM","GLW","EMR","ABNB","USB","MAR","RACE","CRWV","NGG","ITW","PYPL","UPS","TDG","BCS","CP","ITUB","JCI","APO","INFY","VST","RSG","ING","FI","TRI","CNQ","LYG","EPD","DB","CSX","MNST","SPG","NSC","ADSK","AZO","FTNT","TEL","PWR","WDAY","AMX","VRT","ZTS","CL","URI","AEP","TRV","KMI","EQNR","FCX","HLT","COR","DLR","EOG","CNI","SRE","ALNY","AFL","NWG","CMI","REGN","CPNG","APD","AXON","B","DDOG","MPC","TFC","ET","LHX","TRP","FDX","ROP","NXPI","CMG","ALL","MFC","MET","BDX","E","O","FAST","GM","DEO","PSX","NDAQ","PSA","D","LNG","IDXX","VLO","PCAR","EA","ZS","SU","CARR","GRMN","SLB","MPLX","VEEV","XYZ","ARGX","WPM","ROST","XEL","JD","ARES","TTWO","STX","VALE","EXC","BKR","MPWR","GWW","TCOM","AMP","AIG","F","FERG","RKT","IMO","DHI","BIDU","OKE","PAYX","CBRE","KR","TAK","EW","WCN","HEI","WBD","OXY","FLUT","MSCI","FER","CPRT","ETR","CTVA","AME","CCI","FANG","UI","EBAY","FNV","FICO","WDC","CHTR","PEG","HLN","RMD","TGT","HMC","VMC","YUM","CCEP","KMB","A","RDDT","DAL","SYM","HEI.A","TEAM","HSY","GFI","ROK","MLM","ALC","ALAB","CCJ","CAH","SYY","CCL","TKO","ONC","TME","WEC","HIG","ED","AU","PRU","PCG","FIS","XYL","LYV","HUM","LVS","MCHP","OTIS","KDP","TRGP","SMCI","PUK","SLF","ABEV","INSM","IQV","CRCL","HPE","EQT","ACGL","EL","SOFI","CHT","VRSK","GEHC","WAB","CTSH","FIG","STT","VICI","CSGP","WTW","RJF","RKLB","UAL","DD","NRG","IBKR","KVUE","NBIS","CVE","NUE","ASTS","BRO","EXR","RYAAY","STLA","FMX","IR","BBD","MT","VTR","IRM","VG","KGC","PSTG","EME","QSR","KB","LEN","ODFL","ADM","KHC","MTB","WRB","NTR","CLS","DTE","KEYS","FIX","WIT","FITB","EFX","K","TSCO","AWK","TEF","WDS","ERIC","ATO","IX","AEE","ROL","NOK","PPL","BR","VOD","FE","TDY","ES","MTD","MDB","PHG","EXPE","SYF","DXCM","FWONA","FOXA","VIK","AVB","TTD","GIS","LPLA","CRDO","CYBR","CNP","STM","VLTO","TW","CQP","ASX","ULTA","CINF","FOX","FTS","BNTX","CBOE","GRAB","LDOS","FSLR","HPQ"
     );
 
+    public static final List<String> USED_TICKERS = List.of(
+            "AAPL", "TSLA", "AMZN", "MSFT", "NVDA", "GOOGL", "META", "NFLX",
+            "JPM", "V", "BAC", "AMD", "PYPL", "DIS", "T", "PFE", "COST", "INTC",
+            "KO", "TGT", "NKE", "SPY", "BA", "BABA", "XOM", "WMT", "GE", "CSCO",
+            "VZ", "JNJ", "CVX", "PLTR", "SQ", "SHOP", "SBUX", "SOFI", "HOOD",
+            "RBLX", "SNAP", "UBER", "FDX", "ABBV", "ETSY", "MRNA", "LMT", "GM",
+            "F", "RIVN", "LCID", "CCL", "DAL", "UAL", "AAL", "TSM", "SONY", "ET",
+            "NOK", "MRO", "COIN", "SIRI", "RIOT", "CPRX", "VWO", "SPYG", "ROKU",
+            "VIAC", "ATVI", "BIDU", "DOCU", "ZM", "PINS", "TLRY", "WBA", "MGM",
+            "NIO", "C", "GS", "WFC", "ADBE", "PEP", "UNH", "CARR", "FUBO", "HCA",
+            "TWTR", "BILI", "RKT"
+    );
+
     public static void main(String[] args) {
 
         String currentWorkingDirectory = System.getProperty("user.dir");
@@ -54,10 +63,8 @@ public class PaperTraderDataGrabber {
         Gson gson = gsonBuilder.create();
 
         Map<String, CompressedStock> stockMap = new HashMap<>();
-        int number = startingPlace * 25;
-        for (int i = number; i < number + 25; ++i) {
 
-            String symbol = SP_TICKERS.get(i);
+        for (String symbol : USED_TICKERS) {
 
             System.out.println("Starting to create data for " + symbol);
 
@@ -68,10 +75,8 @@ public class PaperTraderDataGrabber {
             }
 
             URLConnection stockData;
-            URLConnection stockOverview;
             try {
-                stockData = getRawStockData(symbol, true);
-                stockOverview = getStockCompanyOverview(symbol);
+                stockData = getRawStockData(symbol);
             } catch (NoSuchURLException e) {
                 System.out.println("URL was not found.");
                 return;
@@ -80,16 +85,14 @@ public class PaperTraderDataGrabber {
                 return;
             }
             String stockJsonData;
-            String overviewJsonData;
             try {
                 stockJsonData = connectionAsString(stockData);
-                overviewJsonData = connectionAsString(stockOverview);
             } catch (CouldNotConvertToStringException e) {
                 System.out.println("Could not convert json to string");
                 return;
             }
 
-            convertToReadableJson(gson, stockMap, symbol, overviewJsonData, stockJsonData);
+            convertToReadableJson(gson, stockMap, symbol, stockJsonData);
         }
 
         try {
@@ -104,25 +107,19 @@ public class PaperTraderDataGrabber {
         }
     }
 
-    private static void convertToReadableJson(Gson gson, Map<String, CompressedStock> stockMap, String symbol, String overviewJsonData, String jsonData) {
+    private static void convertToReadableJson(Gson gson, Map<String, CompressedStock> stockMap, String symbol, String jsonData) {
         JsonElement jsonElement = JsonParser.parseString(jsonData);
-        JsonElement pages = jsonElement.getAsJsonObject().get("Time Series (Daily)");
 
-        if (pages == null) {
-            System.out.println("Json String " + jsonData + " is invalid!");
-            return;
-        } else if (pages.isJsonNull() || !pages.isJsonObject()) {
-            System.out.println("Improper json data " + pages.toString());
-            return;
-        }
+        JsonArray array = jsonElement.getAsJsonArray();
 
-        Set<Map.Entry<String, JsonElement>> entrySet = pages.getAsJsonObject().entrySet();
+        List<JsonElement> elements = array.asList();
 
         ArrayList<Stock> stockData = new ArrayList<>();
 
-        for (Map.Entry<String, JsonElement> map : entrySet) {
-            Stock stock = gson.fromJson(map.getValue(), Stock.class);
-            String time = map.getKey();
+        for (JsonElement element : elements) {
+            Stock stock = gson.fromJson(element, Stock.class);
+
+            String time = stock.date;
 
             int firstHyphen = time.indexOf('-');
             int years = Integer.parseInt(time.substring(0, firstHyphen));
@@ -144,7 +141,7 @@ public class PaperTraderDataGrabber {
             stockData.add(stock);
         }
 
-        stockMap.put(symbol, new CompressedStock(overviewJsonData, stockData));
+        stockMap.put(symbol, new CompressedStock(stockData));
     }
 
     private static int getDaysInMonth(int month, int year) {
@@ -177,16 +174,9 @@ public class PaperTraderDataGrabber {
         return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
 
-    private static URLConnection getRawStockData(String symbol, boolean compact) throws NoSuchURLException, BadConnectionException {
-        String encodedURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY" +
-                "&symbol=" + symbol + "&outputsize=" + (compact ? "compact" : "full") + "&apikey=" + APIKEY;
-
-        return getUrlConnection(encodedURL);
-    }
-
-    private static URLConnection getStockCompanyOverview(String symbol) throws NoSuchURLException, BadConnectionException {
-        String encodedURL = "https://www.alphavantage.co/query?function=OVERVIEW" +
-                "&symbol=" + symbol + "&apikey=" + APIKEY;
+    private static URLConnection getRawStockData(String symbol) throws NoSuchURLException, BadConnectionException {
+        String encodedURL = "https://financialmodelingprep.com/stable/historical-price-eod/full" +
+                "?symbol=" + symbol + "&from=2025-05-1" + "&to=2025-10-1" + "&apikey=" + APIKEY;
 
         return getUrlConnection(encodedURL);
     }
@@ -226,28 +216,20 @@ public class PaperTraderDataGrabber {
 
     public static class Stock {
         long days = 0;
-        @SerializedName(value = "open", alternate = {"1. open"})
+        String date;
         double open = 0d;
-        @SerializedName(value = "close", alternate = {"4. close"})
         double close = 0d;
-        @SerializedName(value = "volume", alternate = {"5. volume"})
         long volume = 0L;
     }
 
     public static class CompressedStock {
 
-        String sector = "";
         double averageGrowth = 0.0;
         double deviation = 0.0;
         double shareValue = 0.0;
         long shares = 0;
 
-        CompressedStock(String overviewJsonData, ArrayList<Stock> stocks) {
-
-            JsonElement jsonElement = JsonParser.parseString(overviewJsonData);
-            JsonElement sect = jsonElement.getAsJsonObject().get("Sector");
-
-            sector = sect.getAsString();
+        CompressedStock(ArrayList<Stock> stocks) {
 
             Stock latestStock = stocks.getFirst();
             shareValue = latestStock.close;
